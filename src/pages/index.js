ef65f080df30168c044fd42256e53eaf788becd2
Syarifah Riefandania <rifiesy@yahoo.com>
2020-07-19 16:50:23 +0700

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+import { Button } from "react-bulma-components";
+
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+    <Button>Bulma Button it is</Button>
+  </div>
 }
